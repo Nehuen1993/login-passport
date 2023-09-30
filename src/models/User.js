32f7899b968d75a702d71f7b649c928a1d6dd4d5
nehuen giannone <n.giannone@hotmail.com>
2023-09-29 21:29:33 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -4,28 +4,28 @@ const userSchema = new mongoose.Schema({
 
     isAdmin: {
         type: Boolean,
-        default: false
+        
       },
 
     first_name: {
         type: String,
-        required: true
+   
     },
     last_name: {
         type: String,
-        required: true
+      
     },
     email: {
         type: String,
-        required: true
+
     },
     age: {
         type: Number,
-        required: true
+      
     },
     password: {
         type: String,
-        required: true
+        
     }
 })
 const User = mongoose.model('users', userSchema)
