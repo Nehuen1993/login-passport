570daf77bb5ad8d9762e081ef44cd006fba32741
nehuen giannone <n.giannone@hotmail.com>
2023-09-29 21:30:11 -0300

    Update passport.config.js

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -9,41 +9,41 @@ const bcrypt = require("bcrypt");
 
 const localStrategy = local.Strategy;
 
-const initializePassport = () => {
-    passport.use(
-        "register",
-        new localStrategy(
-            { passReqToCallback: true, usernameField: "email" },
-            async (req, username, password, done) => {
-                const { first_name, last_name, email, age } = req.body;
-                try {
-                    let user = await userService.findOne({ email: username });
-                    if (user) {
-                        console.log("El usuario ya existe");
-                        return done(null, false);
-                    }
+// const initializePassport = () => {
+//     passport.use(
+//         "register",
+//         new localStrategy(
+//             { passReqToCallback: true, usernameField: "email" },
+//             async (req, username, password, done) => {
+//                 const { first_name, last_name, email, age } = req.body;
+//                 try {
+//                     let user = await userService.findOne({ email: username });
+//                     if (user) {
+//                         console.log("El usuario ya existe");
+//                         return done(null, false);
+//                     }
 
-                    if (!first_name || !last_name || !email || !age || !password) {
+//                     if (!first_name || !last_name || !email || !age || !password) {
                         
-                        console.log("Faltan campos obligatorios");
-                        return done(null, false);
-                    }
+//                         console.log("Faltan campos obligatorios");
+//                         return done(null, false);
+//                     }
 
-                    const newUser = {
-                        first_name,
-                        last_name,
-                        email,
-                        age,
-                        password: await bcrypt.hash(password, 10), 
-                    };
-                    let result = await userService.create(newUser);
-                    return done(null, result);
-                } catch (error) {
-                    return done("Error al obtener el usuario " + error);
-                }
-            }
-        )
-    )
+//                     const newUser = {
+//                         first_name,
+//                         last_name,
+//                         email,
+//                         age,
+//                         password: await bcrypt.hash(password, 10), 
+//                     };
+//                     let result = await userService.create(newUser);
+//                     return done(null, result);
+//                 } catch (error) {
+//                     return done("Error al obtener el usuario " + error);
+//                 }
+//             }
+//         )
+//     )
 
 
 
@@ -76,18 +76,50 @@ const initializePassport = () => {
             }
         })
     );
+
+
+const initializePassport = () => {
+    passport.use("github", new GitHubStrategy({
+        clientID: "Iv1.87de3221720d73ba",
+        clientSecret: "780fd64ba495d08436698c82ef5639aa9266b0e6",
+        callbackURL: "http://localhost:8080/api/sessions/githubcallback"
+
+    }, async (accessToken, refreshToken, profile, done) => {
+        try {
+            let user = await userService.findOne({ email: profile._json.email })
+
+            console.log(user)
+            if (!user) {
+                let newUser = {
+                    first_name: profile._json.name,
+                    last_name: "coder",
+                    age: 18,
+                    email: profile._json.email,
+                    password: ""
+                }
+                let result = await userService.create(newUser)
+                done(null, result)
+            }
+            else {
+                done(null, user)
+            }
+        } catch (error) {
+            return done(error)
+        }
+    }
+    ))
 }
 
-// const cookieExtractor = req => {
-//     let token = null
-//     if (req && req.cookies) {
-//         token = req.cookies["coderCookieToken"]
-//     }
-//     return token
-// }
+const cookieExtractor = req => {
+    let token = null
+    if (req && req.cookies) {
+        token = req.cookies["coderCookieToken"]
+    }
+    return token
+}
 
-// const JWTStrategy = jwt.Strategy
-// const ExtractJWT = jwt.ExtractJwt
+const JWTStrategy = jwt.Strategy
+const ExtractJWT = jwt.ExtractJwt
 
 // const initializePassport = () => {
 //     passport.use("jwt", new JWTStrategy({
